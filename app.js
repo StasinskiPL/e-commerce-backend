63bd2e238f09b264e9c982a8d3674ce67d955e8f
GreenDeer2001 <01DawidS@gmail.com>
2021-01-05 14:36:39 +0100

    h

--- app.js
+++ app.js
@@ -19,11 +19,9 @@ mongoose.connect(
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
 
