9904e177f94f5c9e3eae5e8ff6f9c05207d5bb59
GreenDeer2001 <01DawidS@gmail.com>
2021-01-05 15:12:28 +0100

    bug fixed

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 8080;
 const productRoute = require("./routes/Products");
 
 app.use(express.json());
-app.use("/", productRoute);
 
 mongoose.connect(
   "mongodb+srv://dawid:dawid@shop.j0r2p.mongodb.net/Shop?retryWrites=true&w=majority",
@@ -14,19 +13,19 @@ mongoose.connect(
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
-);
+  );
+  
+  app.use((req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    next();
+  });
+  
 
 
+  app.use("/", productRoute);
 
-
-
-app.use((req, res, next) => {
-  // res.setHeader("Content-Type", "application/json");
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
 
 app.listen(port);
